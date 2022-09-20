858542029d2775c6665f99169af8c6877f67b9f7
Дарья <yu.dar@yandex.ru>
2022-09-20 14:57:27 +0300

    swiper

--- source/js/main.js
+++ source/js/main.js
@@ -27,19 +27,19 @@ addSmoothScroll();
 onClickTab();
 
 // import Swiper bundle with all modules installed
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
 
 
 // ---------------------------------
